3dc8d0e6c6421e97096484ae242a20527aa50c69
Kevin Sivabalan <jbosshellokitty@gmail.com>
2014-10-26 15:31:13 -0400

    adding available file

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,17 @@
 var torrentStream = require('torrent-stream');
 var gui = require('nw.gui');
 var win = gui.Window.get();
+var http = require('http');
 
 var init = function () {
   var getLink = document.getElementById('getLink');
-  getLink.onclick = getMagnet;
+  getLink.onclick = function (argument) {
+    getMagnet;
+  }
+}
+
+var destroyAllData = function () {
+  
 }
 
 var getMagnet = function () {
@@ -18,14 +25,39 @@ var getMagnet = function () {
 }
 
 var startEngine = function (engine) {
+  var count = 0;
+  var items = document.getElementById('files');
+
   engine.on('ready', function() {
     engine.files.forEach(function(file) {
       console.log('filename:', file.name);
       console.log(file);
+      var newFile = document.createElement("li");
+      newFile.appendChild(document.createTextNode(file.name))
+      newFile.setAttribute("id",count+"-file-"+file.name);
+      newFile.setAttribute("class","file_link");
+      items.appendChild(newFile);
+      ++count;
+
+      newFile.onclick = function () {
+        startStream(this);
+      }
       // var stream = file.createReadStream();
       // stream is readable stream to containing the file content
     });
   });
+
+  win.on('close',function () {
+    this.hide();
+    engine.destroy(function(){
+      this.close(true);
+    });
+  })
+}
+
+var startStream = function (file) {
+  var stream = file.createReadStream();
+  console.log(stream);
 }
 
 document.onreadystatechange = (function () {
