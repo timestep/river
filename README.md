8f93f2de054e750910565dbc5ea4296926e55f66
Kevin Sivabalan <jbosshellokitty@gmail.com>
2014-10-26 16:38:46 -0400

    Modifiying a few things to make it more user proof

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ river
 =====
 
 PeerFlix-GUI
+
+
+An experiment in node-webkit, and torrent-stream;
+
+Inspired by peerflix; I wrote this in as primarily an learning exersise in node and torrent-stream
--- app/js/main.js
+++ app/js/main.js
@@ -2,8 +2,16 @@ var torrentStream = require('torrent-stream');
 var gui = require('nw.gui');
 var win = gui.Window.get();
 var http = require('http');
-
+var _ = require('underscore');
 // gui.App.setCrashDumpDir('')
+
+var mediaExtensions = [
+  'avi','mkv','wmv','mp4'
+]
+
+Array.prototype.last = function () {
+  return this[this.length-1];
+}
 
 var init = function () {
   var getLink = document.getElementById('getLink');
@@ -25,23 +33,39 @@ var getMagnet = function () {
 var startEngine = function (engine) {
   var count = 0;
   var items = document.getElementById('files');
-
+  var description = document.getElementById('description');
+  var itemDescirption = document.getElementById('itemDescirption');
+  itemDescirption.style.display = 'inherit';
   engine.on('ready', function() {
+    document.getElementById('link').style.display = 'none';
     engine.files.forEach(function(file) {
+
+      //TODO: HIDE non media files
+
       console.log('filename:', file.name);
       console.log(file);
-      var newFile = document.createElement("li");
-      newFile.appendChild(document.createTextNode(file.name))
-      newFile.setAttribute("id",count+"-file-"+file.name);
-      newFile.setAttribute("class","file_link");
-      newFile.file = file;
-      items.appendChild(newFile);
-      ++count;
 
-      newFile.onclick = function () {
-        debugger;
-        startStream(this.file);
+      var ext = file.name.split('.').last()
+
+      if(mediaExtensions.indexOf(ext) !== -1){
+        var newFile = document.createElement("li");
+        newFile.appendChild(document.createTextNode(file.name))
+        newFile.setAttribute("id",count+"-file-"+file.name);
+        newFile.setAttribute("class","file_link");
+        newFile.file = file;
+        items.appendChild(newFile);
+        ++count;
+
+        newFile.onclick = function () {
+          //TODO: REmove files from list;
+          items.style.visibility = "hidden";
+          itemDescirption.style.display = 'none';
+          description.style.display = "inherit"
+
+          startStream(this.file);
+        }
       }
+      
       // var stream = file.createReadStream();
       // stream is readable stream to containing the file content
     });
@@ -50,8 +74,11 @@ var startEngine = function (engine) {
 
 var startStream = function (file) {
 
+  //add description how to load the stream;
+
   http.createServer(function (req,res) {
     var stream = file.createReadStream();
+    console.log(stream);
     stream.pipe(res);
   }).listen(1337);
 
